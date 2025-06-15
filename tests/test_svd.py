524a1d465e69cf7315dd583656892cbcfd5923ca
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-15 11:47:03 +0100

    add unit tests for ExactSVD with parameterized matrix types

---
+++ tests/test_svd.py
@@ -0,0 +1,26 @@
+import dask.array as da
+import pytest
+
+from svdrom.svd import ExactSVD
+
+
+class TestSVD:
+    def _make_matrix(self, n_rows, n_cols):
+        self.X = da.random.random((n_rows, n_cols)).astype("float32")
+
+    @pytest.mark.parametrize(
+        ("matrix_type", "n_rows", "n_cols"),
+        [
+            ("tall-and-skinny", 10_000, 100),
+            ("short-and_fat", 100, 10_000),
+            ("square", 1_000, 1_000),
+        ],
+    )
+    def test_exact_svd(self, matrix_type, n_rows, n_cols):
+        self._make_matrix(n_rows, n_cols)
+        if matrix_type == "square":
+            with pytest.raises(RuntimeError):
+                exact_svd = ExactSVD(self.X)
+        else:
+            exact_svd = ExactSVD(self.X)
+            exact_svd.fit(n_components=10)
