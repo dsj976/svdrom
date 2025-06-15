551bcb4ba7fd09d102eb8949753503e23948f4b5
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-15 11:41:23 +0100

    refactor ExactSVD to include rechunking logic based on matrix type

--- src/svdrom/svd.py
+++ src/svdrom/svd.py
@@ -15,9 +15,16 @@ class SVD(ABC):
             logger.exception(msg)
             raise ValueError(msg)
         self.X = X
+        self._check_matrix_type()
 
-    def _rechunk(self, col_chunk_size: int = -1):
-        self.X = self.X.rechunk({0: "auto", 1: col_chunk_size}).persist()
+    def _check_matrix_type(self, aspect_ratio=10):
+        n_rows, n_cols = self.X.shape
+        if (n_rows // n_cols) >= aspect_ratio:
+            self.matrix_type = "tall-and-skinny"
+        elif (n_cols // n_rows) >= aspect_ratio:
+            self.matrix_type = "short-and-fat"
+        else:
+            self.matrix_type = "square"
 
     @abstractmethod
     def fit(self, n_components: int):
@@ -27,15 +34,32 @@ class SVD(ABC):
 class ExactSVD(SVD):
     def __init__(self, X):
         super().__init__(X)
+        msg = (
+            "Will need to rechunk the array before fitting the SVD. "
+            "This will add some overhead."
+        )
+        if (
+            self.matrix_type == "tall-and-skinny"
+            and self.X.shape[1] != self.X.chunksize[1]
+        ):
+            logger.info(msg)
+            self.X = self.X.rechunk({0: "auto", 1: -1})
+        if (
+            self.matrix_type == "short-and-fat"
+            and self.X.shape[0] != self.X.chunksize[0]
+        ):
+            logger.info(msg)
+            self.X = self.X.rechunk({0: -1, 1: "auto"})
+        if self.matrix_type == "square":
+            msg = (
+                "The exact SVD algorithm can only handle tall-and-skinny "
+                "or short-and-fat matrices, i.e. the aspect ratio must be "
+                ">= 10. Try using the randomized SVD algorithm instead."
+            )
+            logger.exception(msg)
+            raise RuntimeError(msg)
 
     def fit(self, n_components):
-        if self.X.shape[1] != self.X.chunksize[1]:
-            msg = (
-                "Will need to rechunk the array before fitting the SVD. "
-                "This will add some overhead."
-            )
-            logger.info(msg)
-            self._rechunk()
         self.X = self.X.persist()
         try:
             logger.info("Fitting exact SVD...")
