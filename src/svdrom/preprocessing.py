8e64d2e5ff3c53ef6c6a6803e160df23fdce57d4
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-07-17 14:24:22 +0100

    build StandardScaler class

--- src/svdrom/preprocessing.py
+++ src/svdrom/preprocessing.py
@@ -1,9 +1,7 @@
 from collections.abc import Sequence
 
-import dask.array as da
 import numpy as np
 import xarray as xr
-from dask_ml.preprocessing import StandardScaler as ss
 
 
 class ArrayPreprocessor:
@@ -40,33 +38,84 @@ class ArrayPreprocessor:
 
         return X.stack(space=dims)
 
-    def variable_stack(self, X: xr.Dataset, variables: Sequence[str]) -> xr.DataArray:
+    def variable_stack(self, X: xr.Dataset, variables: Sequence[str]):
         """Given a xarray.Dataset containing multiple variables,
         return a xarray.DataArray where the specified variables
         have been stacked along the spatial dimension.
 
         Not yet implemented.
         """
-        pass
+        return
 
 
 class StandardScaler(ArrayPreprocessor):
+    """Preprocessing class for scaling Xarray datasets or data arrays
+    by removing the mean and optionally scaling by the standard deviation
+    along a specified dimension.
+
+    Attributes
+    ----------
+    mean (xr.DataArray | xr.Dataset): the mean values computed along the
+    specified dimension.
+    std (xr.DataArray | xr.Dataset): the standard deviation values computed
+    along the specified dimension.
+    with_std (bool): whether the data has been scaled to unit variance.
+    """
+
     def __init__(self):
-        self._mean = np.empty_like(0)
-        self._std = np.empty_like(0)
-        self._with_mean = False
+        self._mean = xr.DataArray(np.zeros_like(0))
+        self._std = xr.DataArray(np.zeros_like(0))
         self._with_std = False
-        self._scaler = None
 
-    def fit(
+    @property
+    def mean(self) -> xr.DataArray | xr.Dataset:
+        """Mean (read-only)."""
+        return self._mean
+
+    @property
+    def std(self) -> xr.DataArray | xr.Dataset:
+        """Standard deviation (read-only)."""
+        return self._std
+
+    @property
+    def with_std(self) -> bool:
+        """Whether data scaled to unit variance or not (read_only)."""
+        return self._with_std
+
+    def scale(
         self,
-        X: da.Array | xr.Dataset | xr.DataArray,
-        dim="time",
-        with_mean: bool = True,
+        X: xr.Dataset | xr.DataArray,
+        dim: str = "time",
         with_std: bool = False,
-        transform: bool = False,
     ):
-        pass
+        """
+        Scales the input xarray Dataset or DataArray by removing the mean
+        and optionally dividing by the standard deviation.
 
-    def transform(self):
-        pass
+        Parameters:
+        -----------
+        X (xr.Dataset | xr.DataArray): the input xarray object to be scaled.
+        dim (str): the dimension along which to compute the mean and
+        standard deviation. Default is "time".
+        with_std (bool): if True, scales the data by dividing by the standard
+        deviation after subtracting the mean. Default is False.
+
+        Returns:
+        --------
+        xr.Dataset | xr.DataArray: the scaled xarray object with the mean removed,
+        and optionally divided by the standard deviation.
+
+        Note
+        ----
+        The mean and standard deviation are computed eagerly and stored as NumPy-backed
+        xarray objects. The returned xarray object is Dask-backed and lazy if the input
+        is Dask-backed.
+        """
+        self._with_std = with_std
+        self._mean = X.mean(dim=dim)
+        self._mean = self._mean.compute()
+        if self._with_std:
+            self._std = X.std(dim=dim)
+            self._std = self._std.compute()
+            return (X - self._mean) / self._std
+        return X - self._mean
