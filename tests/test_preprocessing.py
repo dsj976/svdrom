a9ff3592da6bb5b602bc351eb9295bc2f09250b6
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-07-18 12:04:31 +0100

    finish test for variable_spatial_stack function

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,4 +1,5 @@
 import pytest
+import xarray as xr
 from make_test_data import DataGenerator
 
 from svdrom.preprocessing import variable_spatial_stack
@@ -9,10 +10,48 @@ generator.generate_dataset()
 
 
 @pytest.mark.parametrize("X", [generator.da, generator.ds])
-def test_variable_spatial_stack(X):
+def test_variable_spatial_stack(X: xr.DataArray | xr.Dataset):
+    """Test for the variable_spatial_stack function."""
+    # check that the output is a xarray DataArray
     X_stacked = variable_spatial_stack(X, dims=("x", "y", "z"))
+    assert isinstance(X_stacked, xr.DataArray), (
+        "The result of stacking should be a Xarray DataArray, ",
+        f"got {type(X_stacked).__name__}.",
+    )
+
+    # check that the dimensions are "time" and "samples"
     dims = sorted(map(str, X_stacked.sizes.keys()))
     expected_dims = sorted(["time", "samples"])
     assert (
         dims == expected_dims
     ), f"Expected dimensions after stacking are {expected_dims}, got {dims}."
+
+    # check if shape matches expected shape
+    X_stacked = X_stacked.transpose("samples", "time")
+    len_x, len_y, len_z = len(X.x), len(X.y), len(X.z)
+    if isinstance(X, xr.DataArray):
+        expected_shape = (len_x * len_y * len_z, len(X.time))
+        assert expected_shape == X_stacked.shape, (
+            f"Expected shape for a DataArray after stacking is {expected_shape}, ",
+            f"got {X_stacked.shape}.",
+        )
+    if isinstance(X, xr.Dataset):
+        vars = list(X.keys())
+        expected_shape = (len_x * len_y * len_z * len(vars), len(X.time))
+        assert expected_shape == X_stacked.shape, (
+            f"Expected shape for a Dataset after stacking is {expected_shape}, ",
+            f"got {X_stacked.shape}.",
+        )
+
+    # check that by unstacking we can recover the original
+    X_unstacked = X_stacked.unstack()
+    X = X.transpose("x", "y", "z", "time")
+    if isinstance(X, xr.DataArray):
+        X_unstacked = X_unstacked.transpose("x", "y", "z", "time")
+        assert X.equals(
+            X_unstacked
+        ), "Unstacking should recover the original DataArray."
+    if isinstance(X, xr.Dataset):
+        X_unstacked = X_unstacked.transpose("x", "y", "z", "time", "variable")
+        X_unstacked = X_unstacked.to_dataset(dim="variable")
+        assert X.equals(X_unstacked), "Unstacking should recover the original Dataset."
