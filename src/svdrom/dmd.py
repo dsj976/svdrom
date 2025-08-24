501b9a98080a56034bd8ddcee353e5b451350e3b
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-08-24 11:16:12 +0100

    add time_dimension parameter to OptDMD and create new properties

--- src/svdrom/dmd.py
+++ src/svdrom/dmd.py
@@ -11,12 +11,14 @@ class OptDMD:
     def __init__(
         self,
         n_modes: int = -1,
+        time_dimension: str = "time",
     ) -> None:
         if n_modes != -1 and n_modes < 1:
             msg = "'n_modes' must be a positive integer or -1."
             logger.exception(msg)
             raise ValueError(msg)
         self._n_modes = n_modes
+        self._time_dimension = time_dimension
         self._eigs: np.ndarray | None = None
         self._amplitudes: np.ndarray | None = None
         self._modes: xr.DataArray | None = None
@@ -26,8 +28,27 @@ class OptDMD:
         """Number of DMD modes (read-only)."""
         return self._n_modes
 
-    @staticmethod
-    def _check_svd_inputs(u: xr.DataArray, s: np.ndarray, v: xr.DataArray):
+    @property
+    def time_dimension(self) -> str:
+        """Name of the time dimension (read-only)."""
+        return self._time_dimension
+
+    @property
+    def eigs(self) -> np.ndarray | None:
+        """The DMD eigenvalues (read-only)."""
+        return self._eigs
+
+    @property
+    def modes(self) -> xr.DataArray | None:
+        """The DMD modes (read-only)."""
+        return self._modes
+
+    @property
+    def amplitudes(self) -> np.ndarray | None:
+        """The DMD amplitudes (read-only)."""
+        return self._amplitudes
+
+    def _check_svd_inputs(self, u: xr.DataArray, s: np.ndarray, v: xr.DataArray):
         """Check that the passed SVD results are valid."""
         if not isinstance(u.data, np.ndarray):
             msg = "The left singular vectors have not been computed."
@@ -38,9 +59,19 @@ class OptDMD:
             logger.exception(msg)
             raise ValueError(msg)
         if len(u.components) != len(v.components) or len(u.components) != len(s):
-            msg = "u, s and v must have the same number of components."
+            msg = "'u', 's' and 'v' must have the same number of components."
             logger.exception(msg)
             raise ValueError
+        if self._time_dimension not in v.dims:
+            msg = (
+                f"Specified time dimension {self._time_dimension} not "
+                "a dimension of the right singular vectors 'v'."
+            )
+
+    def _generate_fit_time_vector(self, v: xr.DataArray):
+        """Given the right singular vectors containing the temporal
+        information, generate the time vector for the DMD fit.
+        """
 
     def fit(self, u: xr.DataArray, s: np.ndarray, v: xr.DataArray):
         self._check_svd_inputs(u, s, v)
