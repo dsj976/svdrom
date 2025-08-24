dfefe589257a2d260fa01aa76aea18eabd463199
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-08-24 10:49:09 +0100

    start building fit method for OptDMD

--- src/svdrom/dmd.py
+++ src/svdrom/dmd.py
@@ -1,7 +1,8 @@
 import numpy as np
+import xarray as xr
+from pydmd import BOPDMD
 
 from svdrom.logger import setup_logger
-from svdrom.svd import TruncatedSVD
 
 logger = setup_logger("DMD", "dmd.log")
 
@@ -9,47 +10,50 @@ logger = setup_logger("DMD", "dmd.log")
 class OptDMD:
     def __init__(
         self,
-        tsvd: TruncatedSVD,
         n_modes: int = -1,
     ) -> None:
-        self._check_tsvd(tsvd)
-        self._tsvd = tsvd
-        if n_modes == -1:
-            self._n_modes = tsvd.n_components
-        elif n_modes < 1:
+        if n_modes != -1 and n_modes < 1:
             msg = "'n_modes' must be a positive integer or -1."
+            logger.exception(msg)
             raise ValueError(msg)
-        elif n_modes > tsvd.n_components:
-            msg = (
-                "'n_modes' cannot be greater than the number of "
-                "available SVD components."
-            )
-        else:
-            self._n_modes = n_modes
+        self._n_modes = n_modes
+        self._eigs: np.ndarray | None = None
+        self._amplitudes: np.ndarray | None = None
+        self._modes: xr.DataArray | None = None
 
     @property
     def n_modes(self) -> int:
         """Number of DMD modes (read-only)."""
         return self._n_modes
 
-    @property
-    def tsvd(self) -> TruncatedSVD:
-        """The TruncatedSVD instance used to fit
-        the DMD model (read-only)."""
-        return self._tsvd
-
     @staticmethod
-    def _check_tsvd(tsvd: TruncatedSVD):
-        """Check that the TruncatedSVD instance is valid."""
-        if tsvd.u is None:
+    def _check_svd_inputs(u: xr.DataArray, s: np.ndarray, v: xr.DataArray):
+        """Check that the passed SVD results are valid."""
+        if not isinstance(u.data, np.ndarray):
             msg = "The left singular vectors have not been computed."
+            logger.exception(msg)
             raise ValueError(msg)
-        if not isinstance(tsvd.u.data, np.ndarray):
-            msg = "The left singular vectors have not been computed."
-            raise ValueError(msg)
-        if tsvd.v is None:
+        if not isinstance(v.data, np.ndarray):
             msg = "The right singular vectors have not been computed."
+            logger.exception(msg)
             raise ValueError(msg)
-        if not isinstance(tsvd.v.data, np.ndarray):
-            msg = "The right singular vectors have not been computed."
+        if len(u.components) != len(v.components) or len(u.components) != len(s):
+            msg = "u, s and v must have the same number of components."
+            logger.exception(msg)
+            raise ValueError
+
+    def fit(self, u: xr.DataArray, s: np.ndarray, v: xr.DataArray):
+        self._check_svd_inputs(u, s, v)
+        if self._n_modes > len(s):
+            msg = (
+                "The requested number of DMD modes exceeds the number "
+                "of available SVD components."
+            )
+            logger.exception(msg)
             raise ValueError(msg)
+        if self._n_modes == -1:
+            self._n_modes = len(s)
+
+        bopdmd = BOPDMD(
+            svd_rank=self._n_modes, use_proj=True, proj_basis=u[:, : self._n_modes]
+        )
