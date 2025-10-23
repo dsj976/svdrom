761ff9e6e8284e371633b495aeb576e01f2d2fcc
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-10-23 16:46:23 +0100

    update DataGenerator to use a random number generator for reproducibility

--- tests/make_test_data.py
+++ tests/make_test_data.py
@@ -12,6 +12,7 @@ class DataGenerator:
         z (np.ndarray): 1D array representing the z-coordinate values.
         t (np.ndarray): 1D array representing the time values.
         vars (list): List of variable names to generate data for.
+        seed (int | None): Seed for the random number generator for reproducibility.
         ds (xr.Dataset): Generated xarray Dataset (after calling generate_dataset()).
         da (xr.DataArray): Generated xarray DataArray(after calling
             generate_dataarray()).
@@ -41,19 +42,21 @@ class DataGenerator:
         z: np.ndarray | None = None,
         t: np.ndarray | None = None,
         vars: list | None = None,
+        seed: int | None = 1234,
     ):
         self.x = x if x else np.arange(0, 10)
         self.y = y if y else np.arange(0, 10)
         self.z = z if z else np.arange(0, 5)
         self.t = t if t else np.arange(0, 20)
         self.vars = vars if vars else ["U", "V", "W"]
+        self.rng = np.random.default_rng(seed)
 
     def generate_dataset(self):
         data = {}
         for var in self.vars:
             data[var] = {}
-            data[var]["data"] = np.random.rand(
-                self.x.shape[0], self.y.shape[0], self.z.shape[0], self.t.shape[0]
+            data[var]["data"] = self.rng.random(
+                (self.x.shape[0], self.y.shape[0], self.z.shape[0], self.t.shape[0])
             )
             data[var]["dims"] = ("x", "y", "z", "time")
         data["x"] = {"data": self.x, "dims": ("x")}
@@ -75,8 +78,8 @@ class DataGenerator:
             "time": {"dims": ("time"), "data": self.t},
         }
         data["dims"] = ("x", "y", "z", "time")
-        data["data"] = np.random.rand(
-            self.x.shape[0], self.y.shape[0], self.z.shape[0], self.t.shape[0]
+        data["data"] = self.rng.random(
+            (self.x.shape[0], self.y.shape[0], self.z.shape[0], self.t.shape[0])
         )
         data["name"] = var
         self.da = xr.DataArray.from_dict(data)
